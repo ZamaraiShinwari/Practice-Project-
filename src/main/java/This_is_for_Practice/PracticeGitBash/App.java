62d28adf28a3058209698be10f6bf2938f3daed5
Zamarai Shinwari <fz2890@email.vccs.edu>
2022-07-26 17:02:49 -0400

    This is my new daytime project

---
+++ src/main/java/This_is_for_Practice/PracticeGitBash/App.java
@@ -0,0 +1,13 @@
+package This_is_for_Practice.PracticeGitBash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/classes/JavaPractice.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class JavaPractice {
+	public static void main(String[] args) {
+		
+	
+
+	String str = "Zamarai Shinwari";
+	char[] cha = str.toCharArray();
+	System.out.println(cha);
+		
+
+	}
+}
